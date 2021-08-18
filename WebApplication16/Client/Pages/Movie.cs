5b228aabb2624c959f232b3070eae74cdbb872e0
YATENDRA AWANA <yka6739@gmail.com>
2021-08-18 12:52:56 +0530

    Add project files.

---
+++ WebApplication16/Client/Pages/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication16.Client.Pages
+{
+    public class Movie
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+    }
+}
